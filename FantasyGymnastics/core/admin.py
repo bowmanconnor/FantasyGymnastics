1c2bd1d8c8523c0a2134d21e5503c9079daa045c
Christopher Cosma <cosma.5@osu.edu`>
2021-02-21 03:33:51 -0500

    Added admin search

--- FantasyGymnastics/core/admin.py
+++ FantasyGymnastics/core/admin.py
@@ -6,10 +6,12 @@ from .models import Gymnast, Score
 class GymnastAdmin(admin.ModelAdmin):
     list_display = ('name', 'team')
     list_filter = ('team', 'year')
+    search_fields = ('name', 'team')
 
 class ScoreAdmin(admin.ModelAdmin):
     list_display = ('gymnast', 'meet', 'event', 'score', 'date')
     list_filter = ('meet', 'event')
+    search_fields = ('gymnast__name', 'event')
 
 admin.site.register(Gymnast, GymnastAdmin)
 admin.site.register(Score, ScoreAdmin)
