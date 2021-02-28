19e7c31b4cdb0b6cadfa1e6894996c93e0278468
Production server <connorbowman4@yahoo.com>
2021-02-28 08:56:59 +0000

    Add environment variables

--- FantasyGymnastics/FantasyGymnastics/celery.py
+++ FantasyGymnastics/FantasyGymnastics/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FantasyGymnastics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FantasyGymnastics.settings.production')
 
 app = Celery('FantasyGymnastics')
 app.config_from_object('django.conf:settings', namespace='CELERY')
