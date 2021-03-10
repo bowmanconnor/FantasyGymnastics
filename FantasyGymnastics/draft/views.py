7d1ea45584b0a768e5a3b605098642e9f389ff0d
Connor <connorbowman4@yahoo.com>
2021-03-09 19:50:11 -0500

    Abstracts gymnast search to include file. Puts it in draft.

--- FantasyGymnastics/draft/views.py
+++ FantasyGymnastics/draft/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from core.models import FantasyTeam
+from core.models import FantasyTeam, Gymnast, League
 
 @login_required
 def index(request, league_pk):
     context = {}
     context['league_pk'] = league_pk
     context['teams'] = FantasyTeam.objects.filter(league=league_pk)
+    drafted = League.objects.get(pk=league_pk).drafted.all()
+    context['gymnasts'] = Gymnast.objects.all().exclude(id__in=drafted)
     if FantasyTeam.objects.filter(user=request.user, league=league_pk).exists():
         return render(request, 'draft/draft_test.html', context)
     else:
